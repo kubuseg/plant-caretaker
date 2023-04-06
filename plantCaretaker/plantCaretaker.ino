82f481b490ebc9b4dfe31f653239967c4626285e
Jakub Nitkiewicz <01158207@pw.edu.pl>
2023-04-06 21:13:17 +0200

    Deleted temperature sensor code, changed check time to every whole hour, changed delay time to 500ms

--- plantCaretaker/plantCaretaker.ino
+++ plantCaretaker/plantCaretaker.ino
@@ -1,135 +1,117 @@
 #include <RTClib.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-
-#define CLOCK_INTERRUPT_PIN 2
 
 RTC_DS3231 rtc;
 
-OneWire oneWire(5);
-DallasTemperature tempSensor(&oneWire);
 const int dry = 501; // value for dry sensor
 const int wet = 200; // value for wet sensor
 
 const bool isTest = false;
+const int loopTimeMs = 500;
 const float milliliters = 100.0f;
 const int minHumidity = isTest ? 100 : 40;
 
-const int startHour = isTest ? 0 : 18;
-const int endHour = isTest ? 24 : 22;
-
 //Function for water quantity Q(t)=35t-60 
-const int pumpT = floor((milliliters + 60.0f)/35.0f);
-const TimeSpan pumpTime = TimeSpan(pumpT);
-const TimeSpan checkTime = TimeSpan(0, 1, 0, 0);
+const int pumpTimeSec = floor((milliliters + 60.0f)/35.0f);
+const TimeSpan pumpTimespan = TimeSpan(pumpTimeSec);
+const TimeSpan waitTimespan = TimeSpan(0, 1, 0, 0);
 
 enum AlarmMode {
   Pump,
-  Wait
+  Wait,
+  Idle
 };
-AlarmMode currentAlarmMode = Wait;
+AlarmMode currentAlarmMode = Idle;
 
-double getTemp() {
-  tempSensor.requestTemperatures();
-  double temp = tempSensor.getTempCByIndex(0);
-  return temp;
+void beginRTC() {
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC!");
+    Serial.flush();
+    while (1) delay(10);
+  }
 }
 
-void setAlarm(DateTime dateTime, AlarmMode mode) {
+void setAlarmMode(DateTime dateTime, AlarmMode mode) {
+  currentAlarmMode = mode;
   switch (mode) {
     case Pump:
-      currentAlarmMode = Pump;
-      Serial.println("Changed mode to Pump");
+      Serial.println("Alarm mode set to: Pump");
       Serial.print("Pumping ");
-      Serial.print(pumpT*35-60);
+      Serial.print(pumpTimeSec*35-60);
       Serial.println("ml");
       break;
     case Wait:
-      currentAlarmMode = Wait;
-      Serial.println("Changed mode to Wait");
+      Serial.println("Alarm mode set to: Wait");
+      break;
+    case Idle:
+      Serial.println("Alarm mode set to: Idle");
       break;
   }
+}
 
+void setAlarmTime(DateTime dateTime) {
   //Alarm turns on when hour and minute and seconds math with given dateTime
-  if (!rtc.setAlarm1(
-        dateTime,
-        DS3231_A1_Hour
-      )) {
+  while (!rtc.setAlarm1(dateTime, DS3231_A1_Hour)) {
     Serial.println("Error, alarm wasn't set!");
-  } else {
-    DateTime alarm1 = rtc.getAlarm1();
-    char alarm1Date[10] = "hh:mm:ss";
-    alarm1.toString(alarm1Date);
-    Serial.print("Alarm will happen at: ");
-    Serial.println(alarm1Date);
+    Serial.flush();
+    beginRTC();
+    digitalWrite(6, LOW);
+    delay(10);
   }
+  DateTime alarm1 = rtc.getAlarm1();
+  char alarm1Date[] = "hh:mm:ss";
+  alarm1.toString(alarm1Date);
+  Serial.print("Alarm will happen at: ");
+  Serial.println(alarm1Date);
+}
+
+void setAlarm(DateTime dateTime, AlarmMode mode) {
+  setAlarmMode(dateTime, mode);
+  setAlarmTime(dateTime);
 }
 
 void setup() {
   //Set up Serial communication
   Serial.begin(9600);
+
   //Set pins for soil sensor
   pinMode(A2, OUTPUT);
   digitalWrite(A2, HIGH);
   pinMode(A3, OUTPUT);
   digitalWrite(A3, LOW);
 
-  //Setup for RTC
-  if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC!");
-    Serial.flush();
-    while (1) delay(10);
-  }
+  //Setup for MOS pwm pin
+  pinMode(6, OUTPUT);
 
+  //Setup for RTC
+  beginRTC();
   if (rtc.lostPower()) {
     // this will adjust to the date and time at compilation
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
-
   rtc.disable32K();
-
   rtc.clearAlarm(1);
   rtc.clearAlarm(2);
-
   rtc.writeSqwPinMode(DS3231_OFF);
-
   rtc.disableAlarm(2);
-
   //Set wait alarm
-  setAlarm(rtc.now() + TimeSpan(10), isTest ? Wait : Pump);
-
-  //Setup for temperature sensor
-  tempSensor.begin();
-
-  //Setup for MOS
-  //Pwm pin
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  digitalWrite(7, HIGH);
+  setAlarm(rtc.now() + TimeSpan(10), isTest ? Wait : Idle);
 }
 
 void loop() {
   int startTime = millis();
 
   //Print current time
-  char date[10] = "hh:mm:ss";
+  char date[] = "hh:mm:ss";
   rtc.now().toString(date);
   Serial.print(date);
 
   //Print alarm
   DateTime alarm1 = rtc.getAlarm1();
-  char alarm1Date[10] = "hh:mm:ss";
+  char alarm1Date[] = "hh:mm:ss";
   alarm1.toString(alarm1Date);
   Serial.print(" [Alarm: ");
   Serial.print(alarm1Date);
   Serial.println("]");
-  Serial.println();
-
-  //Get temp
-  double temp = getTemp();
-  Serial.print("Temparature: ");
-  Serial.print(temp);
-  Serial.println("°C");
 
   //Get humidity
   int sensorVal = analogRead(A1);
@@ -141,28 +123,14 @@ void loop() {
   if (rtc.alarmFired(1)) {
     rtc.clearAlarm(1);
     DateTime nowTime = rtc.now();
-    DateTime endTime = DateTime( nowTime.year(),  nowTime.month(),  nowTime.day(), endHour);
-    DateTime startTime = DateTime( nowTime.year(),  nowTime.month(),  nowTime.day(), startHour);
+    DateTime nowHour = DateTime(nowTime.year(),  nowTime.month(),  nowTime.day(), nowTime.hour());
+    DateTime alarmTime = nowHour + waitTimespan;
 
-    DateTime alarmTime = nowTime + checkTime;
-    bool isWateringTime = false;
-    if (alarmTime > endTime) {
-      Serial.println("It's too late for watering");
-      DateTime tommorrow = nowTime + TimeSpan(1, 0, 0, 0);
-      alarmTime = DateTime(tommorrow.year(), tommorrow.month(), tommorrow.day(), startHour);
-    } else if (nowTime < startTime) {
-      Serial.println("It's too early for watering");
-      alarmTime = startTime;
-    } else {
-      isWateringTime = true;
-    }
-    
-    if (isWateringTime && 
-        currentAlarmMode == Wait && 
+    if (currentAlarmMode == Wait && 
         humidity <= minHumidity) {
       //Turn on Pump
       analogWrite(6, 40);
-      setAlarm(rtc.now() + pumpTime, Pump);
+      setAlarm(rtc.now() + pumpTimespan, Pump);
     } else {
       //Turn off Pump
       digitalWrite(6, LOW);
@@ -171,7 +139,15 @@ void loop() {
   }
   
   int endTime = millis();
-  int delayTime = 1000 - (endTime - startTime);
-  //Loop with 1Hz frequency
-  delay(delayTime > 0 ? delayTime : 0);
+  int delayTime = loopTimeMs - (endTime - startTime);
+  if (delayTime < 0) {
+    Serial.print("Failed to loop every ");
+    Serial.print(delayTime);
+    Serial.println("ms");
+    Serial.print("Loop lasted ");
+    Serial.print(endTime - startTime);
+    Serial.println("ms");
+    return;
+  }
+  delay(delayTime);
 }
