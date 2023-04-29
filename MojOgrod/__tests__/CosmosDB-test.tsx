748313efea0bcb44d13712cd5ad88c91774f89e6
Jakub Cimochowski <cimochowski01@gmail.com>
2023-04-29 19:08:36 +0200

    Add initialize() to CosmosDB, fix test1 - initialize database, plantsContainer

--- MojOgrod/__tests__/CosmosDB-test.tsx
+++ MojOgrod/__tests__/CosmosDB-test.tsx
@@ -3,7 +3,3 @@ import CosmosDB from '../src/services/CosmosDB.js';
 test('Create CosmosDB', () => {
   const db = new CosmosDB();
 });
-
-test('Create CosmosDB', () => {
-  const db = new CosmosDB();
-});
--- MojOgrod/src/services/CosmosDB.js
+++ MojOgrod/src/services/CosmosDB.js
@@ -1,14 +1,20 @@
 const { CosmosClient } = require("@azure/cosmos");
 
-const endpoint = 'https://plants-db.documents.azure.com:443/';
-const key = 'x7RlJmLiCD1BMQbCVNpc1Sqhc5LBOlU3q0BQr4DMwwT7GbMPksT0jcrvR4Ywfu55fXT3eZHWh0YxACDbJ01lhw==';
-
 class CosmosDB {
   constructor() {
+    const endpoint = 'https://plants-db.documents.azure.com:443/';
+    const key = 'x7RlJmLiCD1BMQbCVNpc1Sqhc5LBOlU3q0BQr4DMwwT7GbMPksT0jcrvR4Ywfu55fXT3eZHWh0YxACDbJ01lhw==';
     this.client = new CosmosClient({ endpoint, key });
-    this.database = this.client.database('plants-db');
-    this.plantsContainer = this.database.container('Plants');
+    this.database = null;
+    this.plantsContainer = null;
   }
- }
 
- export default CosmosDB;
+  async initialize() {
+    const { database } = await this.client.databases.createIfNotExists({ id: "plantsDB" });
+    const { container } = await database.containers.createIfNotExists({ id: "Plants" });
+    this.database = database;
+    this.plantsContainer = container;
+  }
+}
+
+module.exports = CosmosDB;
