4f242f60421cf9a63473698469c93a53f97bf36a
Jakub Cimochowski <cimochowski01@gmail.com>
2023-04-29 13:45:34 +0200

    Change way of ignoring CosmosDB tests - testPathIgnorePatterns attribute in jest.config.js

--- MojOgrod/__tests__/CosmosDB-test.tsx
+++ MojOgrod/__tests__/CosmosDB-test.tsx
@@ -1,5 +1,9 @@
 import CosmosDB from '../src/services/CosmosDB.js';
 
-test.skip('Create CosmosDB', () => {
+test('Create CosmosDB', () => {
+  const db = new CosmosDB();
+});
+
+test('Create CosmosDB', () => {
   const db = new CosmosDB();
 });
