6dee0e8613d98331640644a4e1acfebbe7fcd681
Jakub Cimochowski <cimochowski01@gmail.com>
2023-05-09 16:41:45 +0200

    Fixed regex to accept dates also in format: "d.mm.yyyy, hh:mm:ss".

--- MojOgrod/__tests__/PlantsDB-test.tsx
+++ MojOgrod/__tests__/PlantsDB-test.tsx
@@ -21,14 +21,11 @@ test('Test getMeasurements', async () => {
   const db = new PlantsDB();
   await db.initialize();
   const measurements = await db.getMeasurements();
-  let id = 0;
-  const dateRegex = /^([0-2]\d|3[01])\.(0\d|1[012])\.\d{4},\s([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/;
+  const dateRegex = /^([1-9]|[0-2]\d|3[01])\.(0\d|1[012])\.\d{4},\s([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/;
 
   measurements.forEach(measurement => {
-    id++;
-    const date = measurement.dateTime
-    console.log(date)
-    const isDateValid = dateRegex.test(date)
+    const date = measurement.dateTime;
+    const isDateValid = dateRegex.test(date);
     expect(isDateValid).toBe(true);
   });
 });
