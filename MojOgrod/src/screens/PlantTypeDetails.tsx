450f698a4dea3cc073bd27219d1c53e3ba4181f3
Jakub Cimochowski <cimochowski01@gmail.com>
2023-05-11 18:38:36 +0200

    Start to work on PlantTypeDetails.tsx

--- MojOgrod/src/screens/PlantTypeDetails.tsx
+++ MojOgrod/src/screens/PlantTypeDetails.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, View, ScrollView, Dimensions } from 'react-native';
+import AppFooter from '../components/AppFooter';
+import BackButton from '../components/BackButton';
+import AppHeader from '../components/AppHeader';
+import appColors from '../styles/appColors';
+
+const screenHeight = Dimensions.get('screen').height;
+const marginSize = screenHeight * 0.025;
+const viewHeight = screenHeight * 0.1;
 
 function PlantDetails({ route }): JSX.Element {
     const pType = route.params.pType;
+    const footerContents = BackButton();
+
     return (
-        <View>
-            <Text>{pType.id}</Text>
-        </View >
+        <SafeAreaView style={{ flex: 1 }}>
+            <AppHeader>{pType.name}</AppHeader>
+            <View style={localStyles.container}>
+                <View style={[localStyles.header, { margin: marginSize, height: viewHeight }]}>
+                    {/*  */}
+                </View>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={[localStyles.scrollView, { margin: marginSize }]}>
+                    {/*  */}
+                </ScrollView>
+            </View>
+            <AppFooter children={footerContents} />
+        </SafeAreaView>
     );
 }
+
+const localStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: appColors.darkerWhite,
+    },
+    header: {
+        backgroundColor: 'white',
+    },
+    scrollView: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+});
 
 export default PlantDetails;
