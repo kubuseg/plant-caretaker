d7e22bc0d4d222538e8e7786e9baa3b1ea92734a
Jakub Cimochowski <cimochowski01@gmail.com>
2023-05-11 18:34:21 +0200

    Add BackButton.tsx

---
+++ MojOgrod/src/components/BackButton.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import appColors from '../styles/appColors';
+import { useNavigation } from '@react-navigation/native';
+import sizes from '../styles/sizes';
+
+function BackButton(): JSX.Element {
+    const navigation = useNavigation();
+
+    return (
+        <TouchableOpacity
+            onPress={() => {
+                navigation.goBack();
+            }}
+            style={backButtonStyles.button}
+        >
+            <Text style={backButtonStyles.buttonText}>POWRÓT</Text>
+        </TouchableOpacity>
+    );
+}
+
+const backButtonStyles = StyleSheet.create({
+    button: {
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        width: sizes.backButtonWidth,
+        height: sizes.backButtonHeight,
+        resizeMode: 'contain',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white', // Biały kolor tła
+        borderRadius: 20,
+    },
+    buttonText: {
+        flex: 1,
+        textAlign: 'center',
+        fontFamily: 'NewTitleRoman',
+        fontSize: 14,
+        color: appColors.turquoise,
+    },
+});
+
+export default BackButton;
