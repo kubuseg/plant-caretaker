f23e27253bc0069c5647efa72097d33bcfec17cc
Jakub Cimochowski <cimochowski01@gmail.com>
2023-05-20 16:43:30 +0200

    Implement animation in UserPlantsSV.

--- MojOgrod/src/components/UserPlantsSV.tsx
+++ MojOgrod/src/components/UserPlantsSV.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef } from 'react';
 import userPlants from '../mocks/userPlants';
 import PlantsSVTemplate from './PlantsSVTemplate';
+import { Animated } from 'react-native';
 
 const plants = userPlants();
 
 const UserPlantsSV = () => {
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+    }).start();
 
     return PlantsSVTemplate({
         plantsList: plants,
         onTouchScreen: '',
+        fadeAnim: fadeAnim,
     });
 }
 
