c8e91544647c1130b29535b045078afb04134f01
Jakub Cimochowski <cimochowski01@gmail.com>
2023-05-21 18:12:29 +0200

    Change useEffect ot useFocusEffect in UserPlantsSV.
    - read list of plants on every focus not mount of element

--- MojOgrod/src/components/UserPlantsSV.tsx
+++ MojOgrod/src/components/UserPlantsSV.tsx
@@ -1,26 +1,27 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { Animated } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import PlantsSVTemplate from './PlantsSVTemplate';
 import JsonFileManager from '../services/JsonFileManager';
 
-function PlantTypesSV(): JSX.Element {
+function UserPlantsSV(): JSX.Element {
     const [plantTypes, setPlantTypes] = React.useState<any[]>([]);
     const fadeAnim = useRef(new Animated.Value(0)).current;
 
-    const readPlants = async () => {
-
-        const result = await JsonFileManager.read('userPlants');
-        setPlantTypes(result);
-        Animated.timing(fadeAnim, {
-            toValue: 1,
-            duration: 500,
-            useNativeDriver: true,
-        }).start();
-    }
-
-    useEffect(() => {
-        readPlants();
-    }, []);
+    useFocusEffect(
+        React.useCallback(() => {
+            const readPlants = async () => {
+                const result = await JsonFileManager.read('userPlants');
+                setPlantTypes(result);
+                Animated.timing(fadeAnim, {
+                    toValue: 1,
+                    duration: 500,
+                    useNativeDriver: true,
+                }).start();
+            };
+            readPlants();
+        }, [fadeAnim]),
+    );
 
     return (
         <PlantsSVTemplate
@@ -31,4 +32,4 @@ function PlantTypesSV(): JSX.Element {
     );
 }
 
-export default PlantTypesSV;
+export default UserPlantsSV;
