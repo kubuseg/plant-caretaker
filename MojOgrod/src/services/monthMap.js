530ac8ad486fe0b304ecffd35c1c1f91a0c3b1fa
Jakub Cimochowski <cimochowski01@gmail.com>
2023-05-22 19:06:36 +0200

    Add monthMap.js.

---
+++ MojOgrod/src/services/monthMap.js
@@ -0,0 +1,15 @@
+const monthMap = new Map();
+monthMap.set(1, 'Styczeń');
+monthMap.set(2, 'Luty');
+monthMap.set(3, 'Marzec');
+monthMap.set(4, 'Kwiecień');
+monthMap.set(5, 'Maj');
+monthMap.set(6, 'Czerwiec');
+monthMap.set(7, 'Lipiec');
+monthMap.set(8, 'Sierpień');
+monthMap.set(9, 'Wrzesień');
+monthMap.set(10, 'Październik');
+monthMap.set(11, 'Listopad');
+monthMap.set(12, 'Grudzień');
+
+export default monthMap;
