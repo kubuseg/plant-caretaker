48dde4130501c2b8aba93a945438d7918bb9ecd1
Eva Reszka <e.reszka@stud.elka.pw.edu.pl>
2023-05-27 19:17:53 +0200

    Update Info Scroll View style

--- MojOgrod/src/components/InformationSVTemplate.tsx
+++ MojOgrod/src/components/InformationSVTemplate.tsx
@@ -9,9 +9,9 @@ import {
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import homeScrollViewStyles from '../styles/homeScrollViewStyle';
+import infoScrollViewStyle from '../styles/infoScrollViewStyle';
 
-const styles = homeScrollViewStyles;
+const styles = infoScrollViewStyle;
 
 type InformationSVParams = {
     infoList: any[];
@@ -27,32 +27,46 @@ function InformationSVTemplate({ infoList, onTouchScreen, fadeAnim }: InfoSVPara
             style={styles.scrollView}>
             <Animated.View style={{ opacity: fadeAnim }}>
                 <View style={styles.container}>
-                    <View style={styles.cardInner}>
+                    <View style={styles.card}>
                         <Image style={styles.icon}
                                source={require('../../assets/info-icons/tap-icon.png')}
                         />
                         <Text style={styles.title}>WODA</Text>
-                        <View style={styles.arrowContainer}>
+                        <View style={styles.alertContainer}>
                               <Image
-                                   style={styles.arrowIcon}
+                                   style={styles.alertIcon}
                                    source={infoList.waterLevelAlarm ?
                                    require('../../assets/info-icons/alert-icon.png')
                                    : require('../../assets/info-icons/okay-icon.png')}
                               />
                         </View>
                     </View>
-                          <View style={styles.cardInner}>
+                    <View style={styles.card}>
                           <Image style={styles.icon}
                                  source={require('../../assets/info-icons/bag-icon.png')}
                           />
                           <Text style={styles.title}>NAWÓZ</Text>
-                          <View style={styles.arrowContainer}>
+                          <View style={styles.alertContainer}>
                                 <Image
-                                      style={styles.arrowIcon}
+                                      style={styles.alertIcon}
                                       source={infoList.fertilizerLevelAlarm ?
                                       require('../../assets/info-icons/alert-icon.png')
                                       : require('../../assets/info-icons/okay-icon.png')}
                                       />
+                          </View>
+                    </View>
+                    <View style={styles.card}>
+                          <Image style={styles.icon}
+                                 source={require('../../assets/info-icons/chip-icon.png')}
+                          />
+                          <Text style={styles.title}>POŁĄCZENIE ZE STEROWANIEM</Text>
+                          <View style={styles.alertContainer}>
+                                <Image
+                                     style={styles.alertIcon}
+                                     source={1 == 2 ? // TODO
+                                      require('../../assets/info-icons/wifi-icon.png')
+                                      : require('../../assets/info-icons/alert-icon.png')}
+                                />
                           </View>
                     </View>
                 </View>
---
+++ MojOgrod/src/styles/infoScrollViewStyle.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import appColors from './appColors';
+import sizes from './sizes';
+
+const infoScrollViewStyle = StyleSheet.create({
+    scrollView: {
+        backgroundColor: appColors.darkerWhite
+    },
+    container: {
+        flexDirection: 'column',
+        marginVertical: sizes.mainViewMargin,
+        alignItems: 'center',
+    },
+    card: {
+        marginVertical: sizes.mainViewMargin,
+        width: sizes.mainViewElementWidth,
+        height: sizes.SVelementHeight,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        backgroundColor: 'white',
+        elevation: 4,
+    },
+    icon: {
+        width: sizes.SVelementHeight*0.6,
+        height: sizes.SVelementHeight*0.6,
+        resizeMode: "contain",
+        marginLeft: 15,
+        marginRight: 15
+    },
+    alertIcon: {
+        width: sizes.SVelementHeight*0.5,
+        height: sizes.SVelementHeight*0.5,
+        resizeMode: "contain",
+        marginRight: 15
+    },
+    title: {
+        fontSize: 16,
+        marginLeft: 10,
+        color: 'black',
+        fontFamily: 'OpenSans-Bold',
+        maxWidth: 130
+    },
+    alertContainer: {
+        flex: 1,
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+});
+
+export default infoScrollViewStyle;
